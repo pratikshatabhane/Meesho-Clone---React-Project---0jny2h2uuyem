e6582e77b52a27d47af44346bf4d5d36e56c8894
Pratiksha Tabhane <103399336+pratikshatabhane@users.noreply.github.com>
2023-03-20 18:13:01 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Context } from './Context/Context';
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <React.StrictMode>
+    <Context>
+    <App />
+    
+    </Context>
+  </React.StrictMode>
+
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
